420d95b78cdfae022a3e302d7e0ae7ef40a54e34
kakaz-one <bousomusume2410836@gmail.com>
2025-03-24 10:54:30 +0900

    next.jsクリーンアップ

---
+++ src/app/auth/employee/login/page.tsx
@@ -0,0 +1,12 @@
+import styles from '../../../page.module.css'
+
+export default function Page() {
+  return (
+      <div className={styles.container}>
+        <h1>従業員ページ</h1>
+        <div className={styles.buttonContainer}>
+        
+      </div>
+    </div>
+  )
+}
