cf5b0e76c3b26f763a7708abe97581f3018ad02a
kakaz-one <bousomusume2410836@gmail.com>
2025-03-31 08:18:02 +0900

    PrismaClientのインポートをlib/prismaから行うように変更（app/api/auth/admin/login/route.tsのみお試しで変更）

--- src/app/api/auth/admin/login/route.ts
+++ src/app/api/auth/admin/login/route.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
 
 // 従業員情報全件取得のAPI
 export const GET = async () => {
